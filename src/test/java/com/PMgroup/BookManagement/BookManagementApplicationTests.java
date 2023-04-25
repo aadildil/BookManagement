6c944e0ee81a0656c3860e98ed28788276ac102a
shabaka computers <aadilvazhakkad@gmail.com>
2023-04-25 23:29:11 +0530

    first commit

---
+++ src/test/java/com/PMgroup/BookManagement/BookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PMgroup.BookManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
